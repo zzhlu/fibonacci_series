b4cbb39537b7aeace72b41fbe0d8687e1b409f29
zhaolu <1632089314@qq.com>
2016-06-10 16:32:26 +0800

    finished

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function fibonacci_series(n) {
-    
-    var fibonacci_series = [0, 1];
-    
+    var fibonacci_series = [0];
+
+    if(n > 0) {
+        fibonacci_series.push(1);
+    }
     for(var i = 2; i <= n; i++) {
         fibonacci_series.push(fibonacci_series[i-1] + fibonacci_series[i-2]);
     }
